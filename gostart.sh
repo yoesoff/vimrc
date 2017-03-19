56eefe461ce0b00e0e19c7645723f8c1e27aadbc
Yusuf Ibrahim <mhyusufibrahim@gmail.com>
2017-03-19 17:33:49 +0700

    Adding Laravel and Go support

---
+++ gostart.sh
@@ -0,0 +1,10 @@
+export GOROOT=/usr/local/go
+export GOBIN=$GOROOT/bin
+export PATH=$PATH:$GOBIN
+export GOPATH=$HOME/go-learn2
+
+echo "GOROOT:" $GOROOT
+echo "GOBIN:" $GOBIN
+echo "GOPATH:" $GOPATH
+
+go version
---
+++ lastart.sh
@@ -0,0 +1,1 @@
+./artisan cache:clear && ./artisan view:clear && ./artisan route:clear && bower install && composer install && composer dump-autoload
